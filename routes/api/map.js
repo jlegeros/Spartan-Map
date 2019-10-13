90bb0ecf3806aed69d5b3c46521433a5b9882f36
jlegeros <jlegeros@gmail.com>
2019-10-13 14:14:56 -0400

    refactored code, added validation, fixed async issue

--- routes/api/map.js
+++ routes/api/map.js
@@ -12,123 +12,85 @@ router.get("/test", (req, res) => res.json({ msg: "Map works" }));
 // @desc    Tests populating the race database...
 // @access  Private
 router.post("/test-get-races", (req, res) => {
-    let msg = "Success?";
-    let count = 0;
-    // cannot find a data feed containing races, but the spartan race page has a JSON object
-    // in its html source containing all their races...  so, I fetch the html source
-    // cut out the parts that I don't need, then parse the string as a JSON object
-    // if the source layout changes, this will need to be updated
-    fetch("https://www.spartan.com/en/race/find-race")
-        .then(res => res.text())
-        .then(body => {
-            let htmlArr = body.split(`<script type="text/javascript">`);
-            htmlArr = htmlArr[1].split(`</script>`);
-            htmlArr = htmlArr[0].split(`window.races = `);
+  let msg = "Success";
+  // cannot find a data feed containing races, but the spartan race page has a JSON object
+  // in its html source containing all their races...  so, I fetch the html source
+  // cut out the parts that I don't need, then parse the string as a JSON object
+  // if the source layout changes, this will need to be updated
+  fetch("https://www.spartan.com/en/race/find-race")
+    .then(res => res.text())
+    .then(body => {
+      let htmlArr = body.split(`<script type="text/javascript">`);
+      htmlArr = htmlArr[1].split(`</script>`);
+      htmlArr = htmlArr[0].split(`window.races = `);
 
-            let jsonRaces = JSON.parse(
-                htmlArr[1].substring(0, htmlArr[1].trim().length - 1)
-            );
+      let jsonRaces = JSON.parse(
+        htmlArr[1].substring(0, htmlArr[1].trim().length - 1)
+      );
 
-            for (var event in jsonRaces) {
-                // if (jsonRaces[event].venue.country === "USA") {
-                if (true) {
-                    for (var subevent in jsonRaces[event].subevents) {
-                        let racetype =
-                            jsonRaces[event].subevents[subevent].category.category_name;
-                        if (
-                            racetype == "Sprint" ||
-                            racetype == "Beast" ||
-                            racetype == "Ultra" ||
-                            racetype == "Stadion" ||
-                            racetype == "Super"
-                        ) {
-                            for (var raceday in jsonRaces[event].subevents[subevent]
-                                .ct_event_id) {
-                                let raceday_id =
-                                    jsonRaces[event].subevents[subevent].ct_event_id[raceday]
-                                        .event;
-                                let day =
-                                    jsonRaces[event].subevents[subevent].ct_event_id[raceday].day;
-                                /*
-                                Race.findOne({ raceday_id })
-                                  .then(race => {
-                                    if (race) {
-                                      // race is already in the system
-                                      console.log(`${race} already exists`);
-                                    } else {
-                                      const newRace = new Race({
-                                        name: jsonRaces[event].event_name,
-                                        racetype:
-                                          jsonRaces[event].subevents[subevent].category
-                                            .category_name,
-                                        event_id:
-                                          jsonRaces[event].subevents[subevent].parent_event_id,
-                                        raceday_id:
-                                          jsonRaces[event].subevents[subevent].ct_event_id[
-                                            raceday
-                                          ].event,
-                                        loc_name:
-                                          jsonRaces[event].subevents[subevent].venue.name,
-                                        loc_address:
-                                          jsonRaces[event].subevents[subevent].venue.address,
-                                        loc_city:
-                                          jsonRaces[event].subevents[subevent].venue.city,
-                                        loc_state:
-                                          jsonRaces[event].subevents[subevent].venue.state,
-                                        loc_zip: jsonRaces[event].subevents[subevent].venue.zip,
-                                        loc_country:
-                                          jsonRaces[event].subevents[subevent].venue.country,
-                                        date: new Date(
-                                          jsonRaces[event].subevents[subevent].start_date
-                                        ),
-                                        day,
-                                        last_updated: Date.now()
-                                      });
-                                      count++;
-                                      newRace
-                                        .save()
-                                        .then()
-                                        .catch(err => console.log(err));
-                                    }
-                                  })
-                                  .catch(err => console.log(err));
-                                  */
-                                const newRace = new Race({
-                                    name: jsonRaces[event].event_name,
-                                    racetype:
-                                        jsonRaces[event].subevents[subevent].category.category_name,
-                                    event_id:
-                                        jsonRaces[event].subevents[subevent].parent_event_id,
-                                    raceday_id:
-                                        jsonRaces[event].subevents[subevent].ct_event_id[raceday]
-                                            .event,
-                                    loc_name: jsonRaces[event].subevents[subevent].venue.name,
-                                    loc_address:
-                                        jsonRaces[event].subevents[subevent].venue.address,
-                                    loc_city: jsonRaces[event].subevents[subevent].venue.city,
-                                    loc_state: jsonRaces[event].subevents[subevent].venue.state,
-                                    loc_zip: jsonRaces[event].subevents[subevent].venue.zip,
-                                    loc_country:
-                                        jsonRaces[event].subevents[subevent].venue.country,
-                                    date: new Date(
-                                        jsonRaces[event].subevents[subevent].start_date
-                                    ),
-                                    day,
-                                    last_updated: Date.now()
-                                });
-                                count++;
-                                newRace
-                                    .save()
-                                    .then()
-                                    .catch(err => console.log(err));
-                            }
-                        }
+      for (var event in jsonRaces) {
+        // if (jsonRaces[event].venue.country === "USA") {
+        if (true) {
+          for (var subevent in jsonRaces[event].subevents) {
+            let spartanEvent = jsonRaces[event].subevents[subevent];
+            let racetype = spartanEvent.category.category_name;
+            if (
+              racetype == "Sprint" ||
+              racetype == "Beast" ||
+              racetype == "Ultra" ||
+              racetype == "Stadion" ||
+              racetype == "Super"
+            ) {
+              for (var raceday in spartanEvent.ct_event_id) {
+                let name = jsonRaces[event].event_name;
+                let event_id = spartanEvent.parent_event_id;
+                let raceday_id = spartanEvent.ct_event_id[raceday].event;
+                let loc_name = spartanEvent.venue.name;
+                let loc_address = spartanEvent.venue.address;
+                let loc_city = spartanEvent.venue.city;
+                let loc_state = spartanEvent.venue.state;
+                let loc_zip = spartanEvent.venue.zip;
+                let loc_country = spartanEvent.venue.country;
+                let date = new Date(spartanEvent.start_date);
+                let day = spartanEvent.ct_event_id[raceday].day;
+
+                Race.findOne({ raceday_id })
+                  .then(race => {
+                    if (race) {
+                      // race is already in the system
+                      // TODO: Diff the data, update if necessary
+                      console.log(`${race.name} already exists`);
+                    } else {
+                      const newRace = new Race({
+                        name,
+                        racetype,
+                        event_id,
+                        raceday_id,
+                        loc_name,
+                        loc_address,
+                        loc_city,
+                        loc_state,
+                        loc_zip,
+                        loc_country,
+                        date,
+                        day,
+                        last_updated: Date.now()
+                      });
+                      newRace
+                        .save()
+                        .then()
+                        .catch(err => console.log(err));
                     }
-                }
+                  })
+                  .catch(err => console.log(err));
+              }
             }
+          }
+        }
+      }
 
-            return res.json({ msg, count });
-        });
+      return res.json({ msg });
+    });
 });
 
 module.exports = router;
