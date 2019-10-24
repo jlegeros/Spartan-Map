cbe5b5214eb98f4ff1820ab5782017be771fbd39
jlegeros <jlegeros@gmail.com>
2019-10-24 11:27:12 -0400

    updated races API to account for the DB change to Events and Races, updated route to get all USA events

--- routes/api/races.js
+++ routes/api/races.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const fetch = require("node-fetch");
 
 const Race = require("../../models/Race");
+const Event = require("../../models/Event");
 
 // @route   POST api/races/test-get-races
 // @desc    Tests populating the race database...
@@ -27,55 +28,71 @@ router.post("/test-get-races", (req, res) => {
       );
 
       for (let event in jsonRaces) {
-        // if (jsonRaces[event].venue.country === "USA") {
-        if (true) {
-          // was filtering by US races, figure I'll just grab it all for now
-          for (let subevent in jsonRaces[event].subevents) {
-            let spartanEvent = jsonRaces[event].subevents[subevent];
-            let racetype = spartanEvent.category.category_name;
-            if (
-              racetype == "Sprint" ||
-              racetype == "Beast" ||
-              racetype == "Ultra" ||
-              racetype == "Stadion" ||
-              racetype == "Super"
-            ) {
-              for (let raceday in spartanEvent.ct_event_id) {
-                let raceday_id = spartanEvent.ct_event_id[raceday].event;
-
-                Race.findOne({ raceday_id })
-                  .then(race => {
-                    if (race) {
-                      // race is already in the system
-                      // TODO: Diff the data, update if necessary
-                      console.log(`${race.name} already exists`);
-                    } else {
-                      // TODO: Fix date based on day for multi day events...
-                      const newRace = new Race({
-                        name: jsonRaces[event].event_name,
-                        racetype,
-                        event_id: spartanEvent.parent_event_id,
-                        raceday_id,
-                        loc_name: spartanEvent.venue.name,
-                        loc_address: spartanEvent.venue.address,
-                        loc_latitude: spartanEvent.venue.latitude,
-                        loc_longitude: spartanEvent.venue.longitude,
-                        loc_city: spartanEvent.venue.city,
-                        loc_state: spartanEvent.venue.state,
-                        loc_zip: spartanEvent.venue.zip,
-                        loc_country: spartanEvent.venue.country,
-                        date: new Date(spartanEvent.start_date),
-                        day: spartanEvent.ct_event_id[raceday].day,
-                        last_updated: Date.now()
-                      });
-                      newRace
-                        .save()
-                        .then()
-                        .catch(err => console.log(err));
-                    }
-                  })
+        for (let subevent in jsonRaces[event].subevents) {
+          let spartanEvent = jsonRaces[event].subevents[subevent];
+          let racetype = spartanEvent.category.category_name;
+          if (
+            racetype == "Sprint" ||
+            racetype == "Beast" ||
+            racetype == "Ultra" ||
+            racetype == "Stadion" ||
+            racetype == "Super"
+          ) {
+            // Make an event here
+            let event_id = spartanEvent.parent_event_id;
+            Event.findOne({ event_id }).then(event => {
+              if (event) {
+                // Event is already in the system
+                // TODO: Diff the data, update if necessary
+                console.log(`${event.name} already exists`);
+              } else {
+                const newEvent = new Event({
+                  name: jsonRaces[event].event_name,
+                  event_id,
+                  loc_name: spartanEvent.venue.name,
+                  loc_address: spartanEvent.venue.address,
+                  loc_latitude: spartanEvent.venue.latitude,
+                  loc_longitude: spartanEvent.venue.longitude,
+                  loc_city: spartanEvent.venue.city,
+                  loc_state: spartanEvent.venue.state,
+                  loc_zip: spartanEvent.venue.zip,
+                  loc_country: spartanEvent.venue.country,
+                  date: new Date(spartanEvent.start_date),
+                  last_updated: Date.now()
+                });
+                newEvent
+                  .save()
+                  .then()
                   .catch(err => console.log(err));
               }
+            });
+            for (let raceday in spartanEvent.ct_event_id) {
+              let raceday_id = spartanEvent.ct_event_id[raceday].event;
+
+              Race.findOne({ raceday_id })
+                .then(race => {
+                  if (race) {
+                    // race is already in the system
+                    // TODO: Diff the data, update if necessary
+                    console.log(`${race.name} already exists`);
+                  } else {
+                    // TODO: Fix date based on day for multi day events...
+                    const newRace = new Race({
+                      name: jsonRaces[event].event_name,
+                      racetype,
+                      event_id: spartanEvent.parent_event_id,
+                      raceday_id,
+                      date: new Date(spartanEvent.start_date),
+                      day: spartanEvent.ct_event_id[raceday].day,
+                      last_updated: Date.now()
+                    });
+                    newRace
+                      .save()
+                      .then()
+                      .catch(err => console.log(err));
+                  }
+                })
+                .catch(err => console.log(err));
             }
           }
         }
@@ -85,19 +102,19 @@ router.post("/test-get-races", (req, res) => {
     });
 });
 
-// @route   GET api/races/
-// @desc    Gets all races in USA
+// @route   GET api/races/events
+// @desc    Gets all race events in USA
 // @access  Public
-router.get("/", (req, res) => {
+router.get("/events", (req, res) => {
   const errors = {};
-  Race.find({ loc_country: "USA" })
+  Event.find({ loc_country: "USA" })
     .sort({ date: 1 })
-    .then(races => {
-      if (!races) {
-        errors.noraces = "There are no races";
+    .then(events => {
+      if (!events) {
+        errors.noevents = "There are no events";
         return res.status(404).json(errors);
       }
-      res.json(races);
+      res.json(events);
     })
     .catch(err => res.status(400).json(err));
 });
